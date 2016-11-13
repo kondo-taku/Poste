16b060f2286855a3a701e123ec5b2f616199e4eb
kondo-taku <tkondo.vb@gmail.com>
2016-11-13 13:43:06 +0900

    修正

--- www/js/dispatch.js
+++ www/js/dispatch.js
@@ -2,16 +2,16 @@ function previewchange(i){
     var objID = document.getElementById('Preview_Box');
     switch(i){
         case 'Item1' : 
-            objID.style.backgroundImage = "url(image/2016-08-17.png)";
+            objID.style.backgroundImage = "url(image/Template_background.png)";
             break;
         case 'Item2' : 
-            objID.style.backgroundImage = "url(image/2016-08-21.png)";
+            objID.style.backgroundImage = "url(image/Template_background_2.png)";
             break;
         case 'Item3' : 
-            objID.style.backgroundImage = "url(image/2016-09-16.png)";
+            objID.style.backgroundImage = "url()";
             break;
         case 'Item4' : 
-            objID.style.backgroundImage = "url(image/2016-11-05.png)";
+            objID.style.backgroundImage = "url()";
             break;
     }
         
